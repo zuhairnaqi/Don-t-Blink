061518bbbeadb7ee3b49efc63a93a8050add57b1
Zuhair Naqi <zuhair.naqi@iomechs.com>
2020-07-12 23:41:49 +0500

    Updated firebase config

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/firebase-firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
